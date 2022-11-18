d22542533dee786ac636e210c6662f1e3f351a3f
梦子 <929546220@qq.com>
2022-11-18 16:36:36 +0800

    添加注释

--- kafka-boot/src/main/java/org/yumeko/kafka/Consumer/MyConsumer.java
+++ kafka-boot/src/main/java/org/yumeko/kafka/Consumer/MyConsumer.java
@@ -2,9 +2,11 @@ package org.yumeko.kafka.Consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public class MyConsumer {
     @KafkaListener(topics = "topic-spring")
     public void onMessage(ConsumerRecord<Integer, String> record) {
